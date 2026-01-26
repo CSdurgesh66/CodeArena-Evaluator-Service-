e3f61149a70beed5c3b787216633aef3a06c65e7
Durgesh Kumar <durgesh42416@gmail.com>
2026-01-26 17:53:44 +0530

    Basic ioredis setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import express from  "express";
 
 import serverConfig from "./config/serverConfig";
+import redisConnection from "./config/redisConfig";
 
 const app = express();
 
-app.listen(serverConfig.PORT,() => {
-    console.log(`server starting at ${serverConfig.PORT}`);
-})
+async function startserver() {
+    try{
+
+        // connect Redis
+        await redisConnection.ping();
+        console.log("Redis is ready");
+
+        // start express server
+        app.listen(serverConfig.PORT, () => {
+            console.log(`Server starting at ${serverConfig.PORT}`);
+        });
+
+    }catch(error){
+        console.error("Failed to start server",error);
+        process.exit(1);
+    }
+}
+
+startserver();
